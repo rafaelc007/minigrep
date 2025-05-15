56f716c3e9083506d9cd495749bb889167679143
Rafael Cardoso Pereira <rafael.cardosop09@gmail.com>
2025-05-15 17:35:34 -0400

    Changed parse args to consume the input.

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,15 @@ struct Cli {
     file_path: String
 }
 
-fn parse_args(args: &Vec<String>) -> Result<Cli, String> {
+fn parse_args(mut args: Vec<String>) -> Result<Cli, String> {
+    // consumes the arguments to avoid unecessary copies
     let  arg_len = args.len();
-    let file_path = if arg_len > 1 {
-        &args[1] } else {return Err(String::from("Missing file path"));};
     let query = if arg_len > 2  {
-        &args[2] } else {
+        args.remove(2) } else {
             iomsg::warn("Missing argument query, entire file will be printed");
-            &String::new()};
+            String::new()};
+    let file_path = if arg_len > 1 {
+        args.remove(1) } else {return Err(String::from("Missing file path"));};
     Ok(
         Cli {
             query: query.clone(),
@@ -30,7 +31,7 @@ fn main() {
     // retieve cli arguments
     let args = std::env::args().collect();
     // parse input arguments here
-    let cli = parse_args(&args);
+    let cli = parse_args(args);     // args moved to cli
     // call run function
     run();
 }
@@ -42,7 +43,7 @@ mod tests {
     #[test]
     fn test_parse_no_query() {
         let args = vec![String::from("filename"), String::from("path")];
-        let result = parse_args(&args).unwrap();
+        let result = parse_args(args).unwrap();
         assert_eq!(result.query, "".to_string());
         assert_eq!(result.file_path, "path".to_string());
     }
@@ -50,7 +51,7 @@ mod tests {
     #[test]
     fn test_parse_full() {
         let args = vec![String::from("filename"), String::from("path"), String::from("query")];
-        let result = parse_args(&args).unwrap();
+        let result = parse_args(args).unwrap();
         assert_eq!(result.query, "query".to_string());
         assert_eq!(result.file_path, "path".to_string());
     }
