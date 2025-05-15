0e2acbe56771676a89170b6e76000ee763fced61
Rafael Cardoso Pereira <rafael.cardosop09@gmail.com>
2025-05-15 18:26:23 -0400

    Finishing logic for the run method.

--- src/main.rs
+++ src/main.rs
@@ -23,17 +23,27 @@ fn parse_args(mut args: Vec<String>) -> Result<Cli, String> {
     )
 }
 
-fn run() {
-
+fn run(cli: &Cli) -> Result<String, std::io::Error>{
+    let data = std::fs::read_to_string(&cli.file_path)?;
+    for s in data[..].split('\n') {
+        if s.contains(&cli.query) {
+            return Ok(String::from(s));
+        }
+    }
+    Err(std::io::Error::new(std::io::ErrorKind::Other,
+        "Failed to find pattern"))
 }
 
 fn main() {
-    // retieve cli arguments
+    // retrieve cli arguments
     let args = std::env::args().collect();
     // parse input arguments here
-    let cli = parse_args(args);     // args moved to cli
+    let cli = parse_args(args).unwrap();     // args moved to cli
     // call run function
-    run();
+    match run(&cli) {
+        Ok(x) => {iomsg::out(&x)},
+        Err(e) => {iomsg::warn(&e.to_string());}
+    };
 }
 
 #[cfg(test)]
@@ -54,5 +64,35 @@ mod tests {
         let result = parse_args(args).unwrap();
         assert_eq!(result.query, "query".to_string());
         assert_eq!(result.file_path, "path".to_string());
+    }
+
+    #[test]
+    fn test_run() {
+        let in_test = InputTest::default();
+        let cli = Cli {
+            query: String::from("has"),
+            file_path: in_test.file_path.clone()};
+        let result = run(&cli).unwrap();
+        assert_eq!(result, String::from("the filthy fox has escaped"))
+
+    }
+
+    struct InputTest {
+        file_path: String
+    }
+
+    impl Drop for InputTest {
+        fn drop(&mut self) {
+            std::fs::remove_file(&self.file_path).unwrap();
+        }
+    }
+    impl Default for InputTest {
+        fn default() -> Self {
+            // create test file
+            let file_path = "test.txt";
+            let write_str = "The red fox going up\nthe black fox going down\nthe filthy fox has escaped";
+            std::fs::write(file_path, write_str).unwrap();
+            Self { file_path: String::from(file_path) }
+        }
     }
 }
